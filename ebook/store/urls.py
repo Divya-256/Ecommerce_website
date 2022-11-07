9e1abdca257416379e09d4e61934625b400ab735
Divya-256 <76658875+Divya-256@users.noreply.github.com>
2022-11-07 22:48:21 +0530

    modified url

--- ebook/store/urls.py
+++ ebook/store/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
    path('',views.store_view),
-   path('cart/',views.cart_view)
+   path('cart/',views.cart_view),
+   path('cart_add_item/',views.add_item)
 ]
--- ebook/store/views.py
+++ ebook/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Ebook
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def store_view(request):
@@ -8,3 +9,11 @@ def store_view(request):
 
 def cart_view(request):
     return render(request,'store\cart.html')
+
+
+@csrf_exempt
+def add_item(request):
+    book_id=request.POST['book_id']
+    title=Ebook.objects.get(pk=book_id).title
+    price=Ebook.objects.get(pk=book_id).price
+    
