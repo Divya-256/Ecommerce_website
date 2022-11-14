99d7c7c9ea0f1db9fd31c87198b918fff59c0f81
Divya-256 <76658875+Divya-256@users.noreply.github.com>
2022-11-14 20:53:39 +0530

    completed

--- ebook/ebook/settings.py
+++ ebook/ebook/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,14 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
-# SECRET_KEY= os.environ[SECRET_KEY]
-# SECRET_KEY = 'django-insecure-_u6!#o+oxf$jn%!$5w0f*=qxfm3-^^d@wgq6g%g)3gqv^hp=nr'
+SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1:8000']
+ALLOWED_HOSTS = []
 
 # Application definition
 
