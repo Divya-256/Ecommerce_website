49d99be067c0e1f6ab6305d2105cafd678ec334b
Divya-256 <76658875+Divya-256@users.noreply.github.com>
2022-11-14 22:13:02 +0530

    deployment initiated

--- ebook/ebook/settings.py
+++ ebook/ebook/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY=config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','']
+ALLOWED_HOSTS = ['127.0.0.1','webcomebook202.herokuapp.com']
 
 # Application definition
 
