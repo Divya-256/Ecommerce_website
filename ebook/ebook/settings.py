88feff274d88172f3acfb2421a542b40ad4b7527
Divya-256 <76658875+Divya-256@users.noreply.github.com>
2023-01-21 22:13:54 +0530

    deployment settings added

--- ebook/ebook/settings.py
+++ ebook/ebook/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY=config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','webcomebook202.herokuapp.com']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 # Application definition
 
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
